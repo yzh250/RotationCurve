a43086e7dba2bf381296172a5ea1a0c722df95bb
yzh250 <65972007+yzh250@users.noreply.github.com>
2020-07-21 20:56:31 -0400

    Add files via upload

--- RC_7443_modified.py
+++ RC_7443_modified.py
@@ -9,7 +9,7 @@ from astropy.table import QTable
 
 from scipy.optimize import minimize
 
-from velocity_functions import nloglike_Bur, v_co_Burket, v_co_Burket_nb,v_d, vel_h_Burket, vel_b, nloglike_Bur_nb
+from rotation_curve_functions import nloglike_Bur, v_co_Burket, v_co_Burket_nb,v_d, vel_h_Burket, vel_b, nloglike_Bur_nb, RC_fitting, RC_plotting
 
 import dynesty
 from dynesty import plotting as dyplot
@@ -55,158 +55,13 @@ for i in range(len(DTable_list)):
     av_err = DTable_list[i]['rot_vel_avg_error'].data
     mav_err = DTable_list[i]['max_velocity_error'].data
     miv_err = DTable_list[i]['min_velocity_error'].data
-    if list(r)[int(len(r)/2)] != max(list(r)[:int(len(r)/2)]):
-            p0 = [0.5, 200, np.log10(Mass_list[i])+0.5, max(np.array(r))/3, 0.02, max(list(r))*10]
-            param_bounds = [[0.2, 1],  # Scale Factor [unitless]
-                            [0, 1000],  # Bulge Scale Velocity [km/s]
-                            [8, 12],  # Disk mass [log(Msun)]
-                            [0.1, 100],  # Disk radius [kpc]
-                            [0.0001, 10],  # Halo density [Msun/pc^2]
-                            [0.1, 1000]]  # Halo radius [kpc]
-
-            bestfit_av = minimize(nloglike_Bur, p0, args=(r, av, av_err),
-                              bounds=param_bounds)
-            print('---------------------------------------------------')
-            print('Average curve')
-            print(bestfit_av)
-
-            r_normalized = r / bestfit_av.x[1]
-
-            plt.errorbar(r_normalized, av, yerr=av_err, fmt='g*', label='Average')
-            plt.plot(r_normalized, v_co_Burket(np.array(r), bestfit_av.x), '--', label='fit')
-            plt.plot(r_normalized, vel_b(np.array(r) * 1000, bestfit_av.x[0], bestfit_av.x[1], bestfit_av.x[3] * 1000),
-                 label='bulge')
-            plt.plot(r_normalized, v_d(np.array(r) * 1000, 10 ** bestfit_av.x[2], bestfit_av.x[3] * 1000), label='disk')
-            plt.plot(r_normalized, vel_h_Burket(np.array(r) * 1000, bestfit_av.x[4], bestfit_av.x[5] * 1000),
-                 label='Burket halo')
-            plt.legend()
-            plt.xlabel('$r_{dep}$/$R_d$')
-            plt.ylabel('$v_{rot}$ [km/s]')
-            plt.title(galaxy_ID_list[i])
-            plt.show()
-            # -------------------------------------------------------------------------------
-
-            # -------------------------------------------------------------------------------
-            # Positive rotation curve
-            # -------------------------------------------------------------------------------
-            # Initial guesses
-
-            bestfit_mav = minimize(nloglike_Bur, p0, args=(r, mav, mav_err),
-                               bounds=param_bounds)
-            print('---------------------------------------------------')
-            print('Positive curve')
-            print(bestfit_mav)
-
-            r_normalized_mav = r / bestfit_mav.x[1]
-
-            plt.errorbar(r_normalized_mav, mav, yerr=mav_err, fmt='r*', label='data')
-            plt.plot(r_normalized_mav, v_co_Burket(np.array(r), bestfit_mav.x), '--', label='fit')
-            plt.plot(r_normalized_mav, vel_b(np.array(r) * 1000, bestfit_mav.x[0], bestfit_mav.x[1], bestfit_mav.x[3] * 1000),
-                 label='bulge')
-            plt.plot(r_normalized_mav, v_d(np.array(r) * 1000, 10 ** (bestfit_mav.x[2]), bestfit_mav.x[3] * 1000), label='disk')
-            plt.plot(r_normalized_mav, vel_h_Burket(np.array(r) * 1000, bestfit_mav.x[4], bestfit_mav.x[5] * 1000),
-                 label='Burket halo')
-            plt.legend()
-            plt.xlabel('$r_{dep}$/$R_d$')
-            plt.ylabel('$v_{rot}$ [km/s]')
-            plt.title(galaxy_ID_list[i])
-            plt.show()
-            # -------------------------------------------------------------------------------
-
-            # -------------------------------------------------------------------------------
-            # Negative rotation curve
-            # -------------------------------------------------------------------------------
-            bestfit_miv = minimize(nloglike_Bur, p0, args=(r, np.abs(miv), miv_err),
-                               bounds=param_bounds)
-            print('---------------------------------------------------')
-            print('Negative curve')
-            print(bestfit_miv)
-
-            r_normalized_miv = r / bestfit_miv.x[1]
-            plt.errorbar(r_normalized_miv, np.abs(miv), yerr=miv_err, fmt='b*', label='data')
-            plt.plot(r_normalized_miv, v_co_Burket(np.array(r), bestfit_miv.x), '--', label='fit')
-            plt.plot(r_normalized_miv, vel_b(np.array(r) * 1000, bestfit_miv.x[0], bestfit_miv.x[1], bestfit_miv.x[3] * 1000),
-                 label='bulge')
-            plt.plot(r_normalized_miv, v_d(np.array(r) * 1000, 10 ** bestfit_miv.x[2], bestfit_miv.x[3] * 1000), label='disk')
-            plt.plot(r_normalized_miv, vel_h_Burket(np.array(r) * 1000, bestfit_miv.x[4], bestfit_miv.x[5] * 1000),
-                 label='Burket halo')
-            plt.legend()
-            plt.xlabel('$r_{dep}$/$R_d$')
-            plt.ylabel('$v_{rot}$ [km/s]')
-            plt.title(galaxy_ID_list[i])
-            plt.show()
-    else: #(no bulge)
-            p0 = [np.log10(Mass_list[i]), max(np.array(r)) / 3, 0.02, max(list(r))*10]
-            param_bounds = [[8, 12],  # Disk mass [log(Msun)]
-                            [0.1, 100],  # Disk radius [kpc]
-                            [0.001, 10],  # Halo density [Msun/pc^2]
-                            [0.1, 1000]]  # Halo radius [kpc]
-
-            bestfit_av = minimize(nloglike_Bur_nb, p0, args=(r, av, av_err),
-                              bounds=param_bounds)
-            print('---------------------------------------------------')
-            print('Average curve')
-            print(bestfit_av)
-
-            r_normalized = r / bestfit_av.x[1]
-
-            plt.errorbar(r_normalized, av, yerr=av_err, fmt='g*', label='Average')
-            plt.plot(r_normalized, v_co_Burket_nb(np.array(r), bestfit_av.x), '--', label='fit')
-            plt.plot(r_normalized, v_d(np.array(r) * 1000, 10 ** bestfit_av.x[0], bestfit_av.x[1] * 1000), label='disk')
-            plt.plot(r_normalized, vel_h_Burket(np.array(r) * 1000, bestfit_av.x[2], bestfit_av.x[3] * 1000),
-                    label='Burket halo')
-            plt.legend()
-            plt.xlabel('$r_{dep}$/$R_d$')
-            plt.ylabel('$v_{rot}$ [km/s]')
-            plt.title(galaxy_ID_list[i])
-            plt.show()
-            # -------------------------------------------------------------------------------
-
-            # -------------------------------------------------------------------------------
-            # Positive rotation curve
-            # -------------------------------------------------------------------------------
-            # Initial guesses
-
-            bestfit_mav = minimize(nloglike_Bur_nb, p0, args=(r, mav, mav_err),
-                               bounds=param_bounds)
-            print('---------------------------------------------------')
-            print('Positive curve')
-            print(bestfit_mav)
-
-            r_normalized_mav = r / bestfit_mav.x[1]
-
-            plt.errorbar(r_normalized_mav, mav, yerr=mav_err, fmt='r*', label='data')
-            plt.plot(r_normalized_mav, v_co_Burket_nb(np.array(r), bestfit_mav.x), '--', label='fit')
-            plt.plot(r_normalized_mav, v_d(np.array(r) * 1000, 10 ** (bestfit_mav.x[0]), bestfit_mav.x[1] * 1000),
-                 label='disk')
-            plt.plot(r_normalized_mav, vel_h_Burket(np.array(r) * 1000, bestfit_mav.x[2], bestfit_mav.x[3] * 1000),
-                 label='Burket halo')
-            plt.legend()
-            plt.xlabel('$r_{dep}$/$R_d$')
-            plt.ylabel('$v_{rot}$ [km/s]')
-            plt.title(galaxy_ID_list[i])
-            plt.show()
-            # -------------------------------------------------------------------------------
-
-            # -------------------------------------------------------------------------------
-            # Negative rotation curve
-            # -------------------------------------------------------------------------------
-            bestfit_miv = minimize(nloglike_Bur_nb, p0, args=(r, np.abs(miv), miv_err),
-                               bounds=param_bounds)
-            print('---------------------------------------------------')
-            print('Negative curve')
-            print(bestfit_miv)
-
-            r_normalized_miv = r / bestfit_miv.x[1]
-            plt.errorbar(r_normalized_miv, np.abs(miv), yerr=miv_err, fmt='b*', label='data')
-            plt.plot(r_normalized_miv, v_co_Burket_nb(np.array(r), bestfit_miv.x), '--', label='fit')
-            plt.plot(r_normalized_miv, v_d(np.array(r) * 1000, 10 ** bestfit_miv.x[0], bestfit_miv.x[1] * 1000),
-                 label='disk')
-            plt.plot(r_normalized_miv, vel_h_Burket(np.array(r) * 1000, bestfit_miv.x[2], bestfit_miv.x[3] * 1000),
-                 label='Burket halo')
-            plt.legend()
-            plt.xlabel('$r_{dep}$/$R_d$')
-            plt.ylabel('$v_{rot}$ [km/s]')
-            plt.title(galaxy_ID_list[i])
-            plt.show()
+    # If the object has a bulge
+    # Plot from 0 to 5 times disk radii
+    # Average Velocity
+    av_fit = RC_fitting(r,Mass_list[i],av,av_err)
+    RC_plotting(r,av,av_err,av_fit,galaxy_ID_list[i])
+    mav_fit = RC_fitting(r,Mass_list[i],mav,av_err)
+    RC_plotting(r,mav,mav_err,mav_fit,galaxy_ID_list[i])
+    miv_fit = RC_fitting(r,Mass_list[i],np.abs(miv),miv_err)
+    RC_plotting(r,np.abs(miv),miv_err,miv_fit,galaxy_ID_list[i])
 ################################################################################
