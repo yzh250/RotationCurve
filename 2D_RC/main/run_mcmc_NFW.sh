dbdaf984afae610d39611871fbda60a952589e2e
Yifan Zhang <yzh250@bluehive.bluehive.circ.private>
2021-11-15 02:04:59 -0500

    pycache

---
+++ 2D_RC/main/run_mcmc_NFW.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#SBATCH -p standard
+#SBATCH --mem=10gb
+#SBATCH --time=60:00:00
+#SBATCH -o /scratch/yzh250/Documents/UR_Stuff/Research_UR/RotationCurve/2D_RC/main/7443_12705_mcmc_NFW_log.log                   
+#SBATCH -e /scratch/yzh250/Documents/UR_Stuff/Research_UR/RotationCurve/2D_RC/main/7443_12705_mcmc_NFW_err.err             
+                                                                                                                                
+module load anaconda3/2020.11
+
+srun python vel_map_MCMC_NFW.py
