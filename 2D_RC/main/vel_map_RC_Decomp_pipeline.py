cc51bc9fc9c07e0eff389acae8d1a97b2c835f84
yzh250 <65972007+yzh250@users.noreply.github.com>
2022-03-02 12:27:47 -0500

    minor change

--- 2D_RC/main/vel_map_RC_Decomp_pipeline.py
+++ 2D_RC/main/vel_map_RC_Decomp_pipeline.py
@@ -113,11 +113,11 @@ r50_ang = DTable['nsa_elpetro_th50_r']
 ################################################################################
 # Obtaining information for MaNGA galaxies
 #-------------------------------------------------------------------------------
-galaxy_ID = []
-plateifu = DTable['plateifu'].data
+galaxy_ID = ['8554-12701']
+#plateifu = DTable['plateifu'].data
 
-for i in range(len(plateifu)):
-    galaxy_ID.append(str(plateifu[i],'utf-8'))
+#for i in range(len(plateifu)):
+#    galaxy_ID.append(str(plateifu[i],'utf-8'))
 
 #-------------------------------------------------------------------------------
 #plate = ['7443','7495','7815','7957','7958','7960','7962','7964','7968','7972','7975','7977','7990','7991','7992']
@@ -426,7 +426,7 @@ for i in range(len(galaxy_ID)):
             NFW_fit = np.ndarray.tolist(NFW_fit)
             Burket_fit = np.ndarray.tolist(Burket_fit)
             
-            #plot_diagnostic_panel(galaxy_ID[i], gshape, scale, Isothermal_fit, NFW_fit, Burket_fit, data_maps['Ha_vel_mask'], data_maps['vmasked'], data_maps['ivar_masked'])
+            plot_diagnostic_panel(galaxy_ID[i], gshape, scale, Isothermal_fit, NFW_fit, Burket_fit, data_maps['Ha_vel_mask'], data_maps['vmasked'], data_maps['ivar_masked'])
             #plot_diagnostic_panel(galaxy_ID[i], gshape, scale, Isothermal_fit, NFW_fit, Burket_fit, Ha_vel_mask, vmasked, ivar_masked)
             ####################################################################
 
