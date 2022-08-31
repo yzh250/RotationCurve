0a422d243773e0ae3f7124c248c0d40dd95bfeec
yzh250 <65972007+yzh250@users.noreply.github.com>
2022-08-30 22:18:37 -0400

    test

--- 2D_RC/main/vel_map_RC_MCMC_iso.py
+++ 2D_RC/main/vel_map_RC_MCMC_iso.py
@@ -97,7 +97,7 @@ fit_mini_iso = ascii.read(fit_mini_iso_name,'r')
 ################################################################################
 galaxy_ID = []
 #plateifu = DTable['plateifu'].data
-plateifu = ['10001-1902']
+plateifu = ['10001-3701']
 
 '''
 for i in range(len(plateifu)):
